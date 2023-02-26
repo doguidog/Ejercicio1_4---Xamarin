f44caa5fe0ebf1db3d88f5ab8bbfeedeb01419b1
Marvin Josue Martinez Zapata <101903394+doguidog@users.noreply.github.com>
2023-02-25 21:55:22 -0600

    Add project files.

---
+++ Ejercicio1_4/Ejercicio1_4/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Ejercicio1_4.Controllers;
+using Ejercicio1_4.Views;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio1_4
+{
+    public partial class App : Application
+    {
+        static EmpleadosDB basedatos;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ListarEmpleados());
+        }
+        public static EmpleadosDB BaseDatos
+        {
+            get
+            {
+                if (basedatos == null)
+                {
+                    basedatos = new EmpleadosDB(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "EmpleadosDB.db3"));
+                }
+                return basedatos;
+            }
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
